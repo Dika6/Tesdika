7b341aa33c4b7c52015be9d347d44d27478e28c5
Dika6 <92838454+Dika6@users.noreply.github.com>
2021-10-23 11:41:55 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,9 +1,9 @@
 function handler(m) {
-  this.sendContact(m.chat, '60199782326', 'Itsuki-san', m)
+  this.sendContact(m.chat, '6281252848955', 'Nando', m)
 }
-handler.help = ['owner', 'creator', 'itsukibotowner']
+handler.help = ['owner', 'creator', 'sholehowner']
 handler.tags = ['info']
 
-handler.command = /^(owner|creator|itsukibotowner)$/i
+handler.command = /^(owner|creator|sholehowner)$/i
 
 module.exports = handler
